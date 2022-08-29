6f72e68df6b9e06b0519edd2f0487af5826cd681
trungnguyen29775 <ITITIU20332@student.hcmiu.edu.vn>
2022-08-29 22:20:58 +0700

    add link music

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let i=0;
 
 const listSong = [
     {
-        "name":"Shay nắng - Amee",
+        "name":"Shay nắng - AMEE",
         "link":"https://tainhacmienphi.biz/get/song/api/403015",
         "poster":"https://images.genius.com/d64785e525d77073b3001376411ddfa4.1000x1000x1.webp"
     },
@@ -19,7 +19,48 @@ const listSong = [
         "name":"Chìm sâu - RPT MCK x Trung Trần",
         "link":"https://tainhacmienphi.biz/get/song/api/366869",
         "poster":"https://nhachot.vn/wp-content/uploads/2022/03/loi-bai-hat-chim-sau-rpt-mck-x-trung-tran-kem-hop-am.jpg"
-    }
+    },
+    {
+        "name":"Anh Biết - Nger(MCK)",
+        "link":"https://tainhacmienphi.biz/get/song/api/215508",
+        "poster":"https://scontent.fsgn2-3.fna.fbcdn.net/v/t39.30808-6/294514626_1679416932420255_6624298566995376701_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=RtHPU70PgxsAX8RUN3m&_nc_ht=scontent.fsgn2-3.fna&oh=00_AT-sFte2oF_AUq2eqSJRFaRB2FacC5t89PaDmgtqPrUtEA&oe=6311035E"
+    },
+    {
+        "name":"Lavie - Wean",
+        "link":"https://tainhacmienphi.biz/get/song/api/217570",
+        "poster":"https://i1.sndcdn.com/avatars-pc20yBfsYHMIlQC0-zEpkFA-t500x500.jpg"
+    },
+    {
+        "name":"Retrograde - Wean, Naomi",
+        "link":"https://tainhacmienphi.biz/get/song/api/145675",
+        "poster":"https://i1.sndcdn.com/artworks-000463086828-ybjgww-t500x500.jpg"
+    },
+    {
+        "name":"Mot nguoi vi em - Wean",
+        "link":"https://tainhacmienphi.biz/get/song/api/209434",
+        "poster":"https://i.ytimg.com/vi/vS85YUO2dX4/maxresdefault.jpg"
+    },
+    {
+        "name":"Áo màu xanh - Wean",
+        "link":"https://tainhacmienphi.biz/get/song/api/259990",
+        "poster":"https://i1.sndcdn.com/artworks-AQkBvtoR3URp-0-t500x500.jpg"
+    },
+    {
+        "name":"Love U So - Wean",
+        "link":"https://tainhacmienphi.biz/get/song/api/233258",
+        "poster":"https://i1.sndcdn.com/artworks-AQkBvtoR3URp-0-t500x500.jpg"
+    },
+    {
+        "name":"I Don't Know - Wean",
+        "link":"https://tainhacmienphi.biz/get/song/api/235998",
+        "poster":"https://i1.sndcdn.com/artworks-AQkBvtoR3URp-0-t500x500.jpg"
+    },
+    {
+        "name":"Tay To - Rapital; MCK; RPT PhongKhin",
+        "link":"https://tainhacmienphi.biz/get/song/api/304258",
+        "poster":"https://i1.sndcdn.com/artworks-huxHg5W9USkzL7t2-jl8Mfg-t500x500.jpg"
+    },
+
 ];
 
 var listSongClone = listSong;
@@ -76,6 +117,7 @@ function previousMusic()
     i-=1;
     if(i==-1)
         i=listSongClone.length-1;
+    posterMusic.src=listSongClone[i].poster;
     titleMusic.textContent=listSongClone[i].name;
     audio.pause();
     audio.src= listSongClone[i].link;
