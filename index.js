48e9c316dc22d2e5fa7e6d6e87f8439bec525836
trungnguyen29775 <ITITIU20332@student.hcmiu.edu.vn>
2022-08-29 09:41:38 +0700

    done icon audio

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const audio = document.querySelector('audio');
-console.log(audio);
+const pauseButton = document.querySelector('.ti-control-pause')
+const playButton = document.querySelector('.ti-control-play')
+const nextButton = document.querySelector('.ti-control-skip-forward')
+const backButton = document.querySelector('.ti-control-skip-backward')
+function pauseMusic()
+{
+    audio.pause();
+    pauseButton.classList.add('hide')
+    playButton.classList.remove('hide')
+}
+
+function playMusic()
+{
+    audio.play();
+    pauseButton.classList.remove('hide')
+    playButton.classList.add('hide')
+}
+
+function nextMusin()
+{
+    audio
+}
+
 audio.volume=0.3;
+
+pauseButton.addEventListener('click',pauseMusic)
+playButton.addEventListener('click',playMusic)
